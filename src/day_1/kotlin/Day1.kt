ab346b6612e110775ac31aeb752dcedb07d80011
Nxllpointer <Nxllpointer@users.noreply.github.com>
2022-12-01 16:37:53 +0100

    Day 1

---
+++ src/day_1/kotlin/Day1.kt
@@ -0,0 +1,43 @@
+// AOC Day 1
+
+data class Elf(var totalCalories: Int)
+
+fun part1(elves: Collection<Elf>) {
+    val elfWithMostCalories =
+        elves.maxByOrNull { it.totalCalories } ?: throw IllegalArgumentException("No elf provided")
+
+    val topElfTotalCalories = elfWithMostCalories.totalCalories
+
+    println("Part 1: The elf with the most amount of calories carrys a total of $topElfTotalCalories calories")
+}
+
+fun part2(elves: Collection<Elf>) {
+    val elvesWithMostCalories = elves.sortedByDescending { it.totalCalories }.take(3)
+    check(elvesWithMostCalories.size >= 3) { "Not enough elves provided" }
+
+    val topElvesTotalCalories = elvesWithMostCalories.sumOf { it.totalCalories }
+
+    println("Part 2: The top 3 calorie carrying elves have a total of $topElvesTotalCalories calories")
+}
+
+fun main() {
+    val elves = getAOCInput { rawInput ->
+        val elves = mutableSetOf<Elf>()
+
+        rawInput
+            .trim()
+            .split("\n\n")
+            .forEach { rawElfCaloriesList ->
+                val elfCalories = rawElfCaloriesList
+                    .split("\n")
+                    .sumOf { it.toInt() }
+
+                elves += Elf(elfCalories)
+            }
+
+        elves
+    }
+
+    part1(elves)
+    part2(elves)
+}
